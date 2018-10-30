f180309c1d2ce7b6c3f7ba88218659c8f8d20c86
b4b4r07 <b4b4r07@gmail.com>
2018-10-30 17:33:32 +0900

    Bump version 0.2.0 and update changelog

--- main.go
+++ main.go
@@ -17,7 +17,7 @@ import (
 const (
 	name        = "tfnotify"
 	description = "Notify the execution result of terraform command"
-	version     = "0.1.4"
+	version     = "0.2.0"
 )
 
 type tfnotify struct {
