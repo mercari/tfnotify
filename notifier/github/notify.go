6b041c08b02566243b758974d11d8597abb79205
Daisuke Fujita <dtanshi45@gmail.com>
2019-11-06 18:29:09 +0900

    Post destroy warning as a GitHub comment

--- notifier/github/notify.go
+++ notifier/github/notify.go
@@ -22,6 +22,16 @@ func (g *NotifyService) Notify(body string) (exit int, err error) {
 		return result.ExitCode, result.Error
 	}
 
+	_, isPlan := parser.(*terraform.PlanParser)
+	if isPlan {
+		if result.HasDestroy && cfg.WarnDestroy {
+			// Notify destroy warning as a new comment before normal plan result
+			if err = g.notifyDestoryWarning(body, result); err != nil {
+				return result.ExitCode, err
+			}
+		}
+	}
+
 	template.SetValue(terraform.CommonTemplate{
 		Title:   cfg.PR.Title,
 		Message: cfg.PR.Message,
@@ -54,4 +64,29 @@ func (g *NotifyService) Notify(body string) (exit int, err error) {
 		Number:   cfg.PR.Number,
 		Revision: cfg.PR.Revision,
 	})
+}
+
+func (g *NotifyService) notifyDestoryWarning(body string, result terraform.ParseResult) error {
+	cfg := g.client.Config
+	destroyWarningTemplate := g.client.Config.DestroyWarningTemplate
+	destroyWarningTemplate.SetValue(terraform.CommonTemplate{
+		Title:   cfg.PR.DestroyWarningTitle,
+		Message: cfg.PR.DestroyWarningMessage,
+		Result:  result.Result,
+		Body:    body,
+		Link:    cfg.CI,
+	})
+	body, err := destroyWarningTemplate.Execute()
+	if err != nil {
+		return err
+	}
+
+	if err := g.client.Comment.Post(body, PostOptions{
+		Number:   cfg.PR.Number,
+		Revision: cfg.PR.Revision,
+	}); err != nil {
+		return err
+	}
+
+	return nil
 }
