24f8f91323ae42ffde8ba9ed79e9a0dfd478f4b6
Timo Genzmer <timo.genzmer@n26.com>
2020-01-21 11:13:36 +0100

    scan stdin by byte instead of line

--- tee.go
+++ tee.go
@@ -3,10 +3,8 @@ package main
 import (
 	"bufio"
 	"bytes"
-	"fmt"
-	"io"
-
 	"github.com/mattn/go-colorable"
+	"io"
 )
 
 func tee(stdin io.Reader, stdout io.Writer) string {
@@ -15,8 +13,9 @@ func tee(stdin io.Reader, stdout io.Writer) string {
 
 	tee := io.TeeReader(stdin, &b1)
 	s := bufio.NewScanner(tee)
+	s.Split(bufio.ScanBytes)
 	for s.Scan() {
-		fmt.Fprintln(stdout, s.Text())
+		stdout.Write(s.Bytes())
 	}
 
 	uncolorize := colorable.NewNonColorable(&b2)
